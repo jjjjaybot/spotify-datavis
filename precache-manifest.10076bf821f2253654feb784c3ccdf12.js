e5117862be453f3a9f0127371023fe50c06d3d89
Jiazi Cai <caijiazi777@gmail.com>
2020-04-23 23:48:01 -0400

    Updates

---
+++ precache-manifest.10076bf821f2253654feb784c3ccdf12.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33086cab3331acb3e39ca37c7aec0600",
+    "url": "/spotify-ui/index.html"
+  },
+  {
+    "revision": "cf7dab2330ab0b90f463",
+    "url": "/spotify-ui/static/css/main.e4f47533.chunk.css"
+  },
+  {
+    "revision": "70efcc81c569e095d9c3",
+    "url": "/spotify-ui/static/js/2.f07e9f50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spotify-ui/static/js/2.f07e9f50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf7dab2330ab0b90f463",
+    "url": "/spotify-ui/static/js/main.675c69a6.chunk.js"
+  },
+  {
+    "revision": "f85f466611fcccff6270",
+    "url": "/spotify-ui/static/js/runtime-main.4fb669b0.js"
+  },
+  {
+    "revision": "3005871a5a18de80182ce2cf5a7167bd",
+    "url": "/spotify-ui/static/media/home.3005871a.svg"
+  },
+  {
+    "revision": "d464e65a9d45f9b6b3056b0019f63a1b",
+    "url": "/spotify-ui/static/media/library.d464e65a.svg"
+  },
+  {
+    "revision": "78b8b9853183e29bd82d98aafc60340c",
+    "url": "/spotify-ui/static/media/play.78b8b985.svg"
+  },
+  {
+    "revision": "0f6375428d8f094acc24d7e67c4ebcc1",
+    "url": "/spotify-ui/static/media/search.0f637542.svg"
+  }
+]);
