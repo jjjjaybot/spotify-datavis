3ed3fe746b29a610fe9e6b0d1eceb572a9d27726
Jiazi Cai <caijiazi777@gmail.com>
2020-04-27 18:18:08 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {BrowserRouter as Router} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-  <Router>
+  <Router basename="/spotify-ui/">
     <App />
     </Router>
   </React.StrictMode>,
